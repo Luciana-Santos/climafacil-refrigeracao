eb5f062782ada501a63c0bafb2985a2b8508bf0c
Luciana Santos <github@luciana.dev>
2026-02-26 17:55:57 -0300

    FAQ session added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#menu')
 const closeMenu = document.querySelector('#close-menu')
 const nav = document.querySelector('#nav')
+const questions = document.querySelectorAll('.question')
 
 menu.addEventListener('click', () => {
   nav.classList.add('menu-ativo')
@@ -8,4 +9,11 @@ menu.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
   nav.classList.remove('menu-ativo')
+})
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-header')
+  btn.addEventListener('click', () => {
+    question.classList.toggle('active')
+  })
 })
